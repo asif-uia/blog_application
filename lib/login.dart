07d620e69093f052a39edf36b857fcbfc10ca3f3
asif.islam <asif07c@gmail.com>
2021-08-16 14:07:18 +0600

    invoked google sign-in mechanism

--- lib/login.dart
+++ lib/login.dart
@@ -27,31 +27,60 @@ class _BodyState extends State<Body> {
   // TextEditingController controller = new TextEditingController(); // 4
 
   void click() {
-    this.name = controller.text;
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => MyHomePage(this.name)));
+    // this.name = controller.text; // 4
+    signInWithGoogle().then((user) => {
+          // this.user = user, // 5
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => MyHomePage(user.displayName)))
+        });
+  }
+
+  Widget googleLoginButton() {
+    // ignore: deprecated_member_use
+    return OutlineButton(
+      onPressed: this.click,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
+      splashColor: Colors.grey,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(image: AssetImage('assets/google_logo.png'), height: 35),
+              Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text('Sign In with Google',
+                      style: TextStyle(color: Colors.grey, fontSize: 25))),
+            ],
+          )),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Align(
         alignment: Alignment.center,
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: TextField(
-            controller: this.controller,
-            decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person),
-                labelText: "Type Your Name",
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(width: 5, color: Colors.black)),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.done),
-                  splashColor: Colors.blue,
-                  tooltip: "Submit",
-                  onPressed: this.click,
-                )),
-          ),
-        ));
+        // child: Padding( // 4
+        //   padding: EdgeInsets.all(10),
+        //   child: TextField(
+        //     controller: this.controller,
+        //     decoration: InputDecoration(
+        //         prefixIcon: Icon(Icons.person),
+        //         labelText: "Type Your Name",
+        //         border: OutlineInputBorder(
+        //             borderSide: BorderSide(width: 5, color: Colors.black)),
+        //         suffixIcon: IconButton(
+        //           icon: Icon(Icons.done),
+        //           splashColor: Colors.blue,
+        //           tooltip: "Submit",
+        //           onPressed: this.click,
+        //         )),
+        //   ),
+        // )
+        child: googleLoginButton());
   }
 }
